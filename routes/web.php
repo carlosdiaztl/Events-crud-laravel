257afc6b6f7ff1e3a20348d376eb9f80f81516ab
carlosDiaz <diaz-alzate@hotmail.com>
2023-04-01 11:11:47 -0500

    test

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,21 @@ use Illuminate\Support\Facades\Auth;
 Route::get('/', function () {
     return view('welcome');
 });
-route::resource('events', App\Http\Controllers\EventController::class);
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/users/{user}/events/{event}/reservar', [App\Http\Controllers\ReservaController::class, 'create'])->name('reservas.create');
-Route::post('/users/{user}/events/{event}/reservar', [App\Http\Controllers\ReservaController::class, 'store'])->name('reservas.store');
-Route::get('/reservas/{user}/{event}', [App\Http\Controllers\ReservaController::class, 'show'])->name('reservas.show');
-Route::get('/reservas/{user}', [App\Http\Controllers\ReservaController::class, 'index'])->name('reservas.index');
-Route::delete('/reservas/destroy/{event}', [App\Http\Controllers\ReservaController::class, 'destroy'])->name('reservas.destroy');
+Auth::routes(['verify' => true]);
+
+
+
+
+Route::middleware('auth', 'verified')->group(
+    function () {
+        route::resource('events', App\Http\Controllers\EventController::class);
+        Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+        Route::get('/users/{user}/events/{event}/reservar', [App\Http\Controllers\ReservaController::class, 'create'])->name('reservas.create');
+        Route::post('/users/{user}/events/{event}/reservar', [App\Http\Controllers\ReservaController::class, 'store'])->name('reservas.store');
+        Route::get('/reservas/{user}/{event}', [App\Http\Controllers\ReservaController::class, 'show'])->name('reservas.show');
+        Route::get('/reservas/{user}', [App\Http\Controllers\ReservaController::class, 'index'])->name('reservas.index');
+        Route::delete('/reservas/destroy/{event}', [App\Http\Controllers\ReservaController::class, 'destroy'])->name('reservas.destroy');
+    }
+
+);
